6d7811a9a735485792a17448fbcbd833c1796ecf
trainerherp <justinorleanwidya@yahoo.com>
2021-06-08 12:18:25 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,127 @@
+/* Register */
+let email = document.getElementById("email")
+let username = document.getElementById("username")
+let password = document.getElementById("password")
+let confirm_password = document.getElementById("confirm-password")
+let terms = document.getElementById("terms")
+
+function validateEmail(email){
+    let emailError = document.getElementById("email-error");
+    if(email.length==0){
+        emailError.innerHTML = "Input your email!"
+        return false;
+    }
+    else if(email[0] == '.'||email[0] == '/'||email[0] == '_'||email[0] == '@'){
+        emailError.innerHTML = "Invalid email!"
+        return false;
+    }
+    else if(email.indexOf('@') == -1){
+        emailError.innerHTML = "Invalid email!"
+        return false;
+    }
+    else if(email[email.indexOf('@')-1] == '.'||email[email.indexOf('@')+1] == '.'){
+        emailError.innerHTML = "Invalid email!"
+        return false;
+    }
+    else if(email.endsWith(".com") == false && email.endsWith(".ac.id") == false){
+        emailError.innerHTML = "Invalid email!"
+        return false;
+    }
+    emailError.innerHTML = "";
+    return true;
+}
+
+function validateUsername(username){
+    if(username.length < 5){
+        document.getElementById("username-error").innerHTML = 
+        "Username must be at least 5 characters in length!";
+        return false;
+    }
+    document.getElementById("username-error").innerHTML = "";
+    return true;
+}
+
+function validatePassword(password){
+    let check = false;
+    for(let i = 0; i < password.length; i++){
+        if(password[i] >= '0' && password[i] <= '9'){
+            check = true;
+            break;
+        }
+    }
+    if(password.length < 8){
+        document.getElementById("password-error").innerHTML = 
+        "Password must be at least 8 characters in length!";
+        return false;
+    }
+    else if(check == false){
+        document.getElementById("password-error").innerHTML = 
+        "Password must contain a number!";
+        return false;
+    }
+    document.getElementById("password-error").innerHTML = "";
+    return true;
+}
+
+function validateConfirmPassword(password, confirm_password){
+    if(password != confirm_password){
+        document.getElementById("confirm-password-error").innerHTML = 
+        "Password doesn't match!";
+        return false;
+    }
+    document.getElementById("confirm-password-error").innerHTML = "";
+    return true;
+}
+
+function validateTerms(terms){
+    if(terms.checked == false){
+        document.getElementById("terms-error").innerHTML =
+        "You must agree to the terms and conditions.";
+        return false;
+    }
+    document.getElementById("terms-error").innerHTML = "";
+    return true;
+}
+
+function submit(){
+    if(validateEmail(email.value) &&
+    validateUsername(username.value) &&
+    validatePassword(password.value) &&
+    validateConfirmPassword(password.value, confirm_password.value) &&
+    validateTerms(terms)
+    ){
+        window.location.href = "../html/index.html";
+        window.alert("Register Success!")
+    }
+}
+
+/* Register End */
+
+/* Gallery */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function moveSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  if(n > slides.length) slideIndex = 1;
+  if(n < 1) slideIndex = slides.length;
+  for(let i = 0; i < slides.length; i++){
+      slides[i].style.display = "none";
+  }
+  for(let i = 0; i < dots.length; i++){
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+/* Gallery End */
